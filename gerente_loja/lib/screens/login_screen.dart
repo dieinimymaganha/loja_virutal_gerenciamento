d4c6d1e1d661172c4b558219a68c0e324a0b6662
Dieinimy <dieinimy@gmail.com>
2020-06-02 19:59:41 -0300

    Iniciada tela de login

---
+++ gerente_loja/lib/screens/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:gerente_loja/widgets/input_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Stack(
+
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.store_mall_directory,
+                    color: Colors.redAccent,
+                    size: 160.0,
+                  ),
+                  InputField(
+                    icon: Icons.person_outline,
+                    hint: 'Usuário',
+                    obscure: false,
+                  ),
+                  InputField(
+                    icon: Icons.lock_outline,
+                    hint: 'Senha',
+                    obscure: true,
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text('Entrar'),
+                      textColor: Colors.white,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
