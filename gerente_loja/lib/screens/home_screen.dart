c2622181cba569aa404d3268b96fbd0b27b87ddd
Dieinimy <dieinimy@gmail.com>
2020-06-03 20:37:19 -0300

    Criada tela de cliente e incluído filtro

--- gerente_loja/lib/screens/home_screen.dart
+++ gerente_loja/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/user_bloc.dart';
 import 'package:gerente_loja/tabs/users_tab.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,10 +12,13 @@ class _HomeScreenState extends State<HomeScreen> {
   PageController _pageController;
   int _page = 0;
 
+  UserBloc _userBloc;
+
   @override
   void initState() {
     super.initState();
     _pageController = PageController();
+    _userBloc = UserBloc();
   }
 
   @override
@@ -34,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 .textTheme
                 .copyWith(caption: TextStyle(color: Colors.white))),
         child: BottomNavigationBar(
-          currentIndex: _page,
+            currentIndex: _page,
             onTap: (p) {
               _pageController.animateToPage(p,
                   duration: Duration(milliseconds: 500), curve: Curves.ease);
@@ -55,18 +60,21 @@ class _HomeScreenState extends State<HomeScreen> {
             ]),
       ),
       body: SafeArea(
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (p) {
-            setState(() {
-              _page = p;
-            });
-          },
-          children: <Widget>[
-            UsersTab(),
-            Container(color: Colors.yellow),
-            Container(color: Colors.green),
-          ],
+        child: BlocProvider<UserBloc>(
+          bloc: _userBloc,
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (p) {
+              setState(() {
+                _page = p;
+              });
+            },
+            children: <Widget>[
+              UsersTab(),
+              Container(color: Colors.yellow),
+              Container(color: Colors.green),
+            ],
+          ),
         ),
       ),
     );
