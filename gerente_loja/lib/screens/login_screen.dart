458bafd5967252e8aefacf9921686ffbdf891b7f
Dieinimy <dieinimy@gmail.com>
2020-06-03 17:23:15 -0300

    Incluido dispose

--- gerente_loja/lib/screens/login_screen.dart
+++ gerente_loja/lib/screens/login_screen.dart
@@ -11,28 +11,34 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final _loginBloc = LoginBloc();
 
-
   @override
   void initState() {
     super.initState();
 
     _loginBloc.outState.listen((state) {
-      switch(state){
+      switch (state) {
         case LoginState.SUCCESS:
-          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => HomeScreen()));
           break;
         case LoginState.FAIL:
-          showDialog(context: context, builder: (context) => AlertDialog(
-            title: Text('Erro'),
-            content: Text('Você não possui os privilégios necessários'),
-          ) );
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: Text('Erro'),
+                    content: Text('Você não possui os privilégios necessários'),
+                  ));
           break;
         case LoginState.LOADING:
         case LoginState.IDLE:
-
       }
     });
+  }
 
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
   }
 
   @override
@@ -43,10 +49,13 @@ class _LoginScreenState extends State<LoginScreen> {
           stream: _loginBloc.outState,
           initialData: LoginState.LOADING,
           builder: (context, snapshot) {
-            switch(snapshot.data){
+            switch (snapshot.data) {
               case LoginState.LOADING:
-                return Center(child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),),);
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
+                  ),
+                );
               case LoginState.FAIL:
               case LoginState.SUCCESS:
               case LoginState.IDLE:
@@ -56,54 +65,56 @@ class _LoginScreenState extends State<LoginScreen> {
                     Container(),
                     SingleChildScrollView(
                         child: Container(
-                          margin: EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Icon(
-                                Icons.store_mall_directory,
-                                color: Colors.pinkAccent,
-                                size: 160,
-                              ),
-                              InputField(
-                                icon: Icons.person_outline,
-                                hint: "Usuário",
-                                obscure: false,
-                                stream: _loginBloc.outEmail,
-                                onChanged: _loginBloc.changeEmail,
-                              ),
-                              InputField(
-                                icon: Icons.lock_outline,
-                                hint: "Senha",
-                                obscure: true,
-                                stream: _loginBloc.outPassword,
-                                onChanged: _loginBloc.changePassword,
-                              ),
-                              SizedBox(height: 32,),
-                              StreamBuilder<bool>(
-                                  stream: _loginBloc.outSubmitValid,
-                                  builder: (context, snapshot) {
-                                    return SizedBox(
-                                      height: 50,
-                                      child: RaisedButton(
-                                        color: Colors.pinkAccent,
-                                        child: Text("Entrar"),
-                                        onPressed: snapshot.hasData ? _loginBloc.submit : null,
-                                        textColor: Colors.white,
-                                        disabledColor: Colors.pinkAccent.withAlpha(140),
-                                      ),
-                                    );
-                                  }
-                              )
-                            ],
+                      margin: EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(
+                            Icons.store_mall_directory,
+                            color: Colors.pinkAccent,
+                            size: 160,
                           ),
-                        )
-                    ),
+                          InputField(
+                            icon: Icons.person_outline,
+                            hint: "Usuário",
+                            obscure: false,
+                            stream: _loginBloc.outEmail,
+                            onChanged: _loginBloc.changeEmail,
+                          ),
+                          InputField(
+                            icon: Icons.lock_outline,
+                            hint: "Senha",
+                            obscure: true,
+                            stream: _loginBloc.outPassword,
+                            onChanged: _loginBloc.changePassword,
+                          ),
+                          SizedBox(
+                            height: 32,
+                          ),
+                          StreamBuilder<bool>(
+                              stream: _loginBloc.outSubmitValid,
+                              builder: (context, snapshot) {
+                                return SizedBox(
+                                  height: 50,
+                                  child: RaisedButton(
+                                    color: Colors.pinkAccent,
+                                    child: Text("Entrar"),
+                                    onPressed: snapshot.hasData
+                                        ? _loginBloc.submit
+                                        : null,
+                                    textColor: Colors.white,
+                                    disabledColor:
+                                        Colors.pinkAccent.withAlpha(140),
+                                  ),
+                                );
+                              })
+                        ],
+                      ),
+                    )),
                   ],
                 );
             }
-          }
-      ),
+          }),
     );
   }
 }
