3f2476d8e8e188ac9c80dfdb2e5a99dca38b6996
Dieinimy <dieinimy@gmail.com>
2020-06-03 17:36:52 -0300

    Criando sistema de paginas

--- gerente_loja/lib/screens/home_screen.dart
+++ gerente_loja/lib/screens/home_screen.dart
@@ -1,8 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.lightBlueAccent,
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: TextStyle(color: Colors.white))),
+        child: BottomNavigationBar(
+          currentIndex: _page,
+            onTap: (p) {
+              _pageController.animateToPage(p,
+                  duration: Duration(milliseconds: 500), curve: Curves.ease);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text('Clientes'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text('Pedidos'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                title: Text('Produtos'),
+              ),
+            ]),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: <Widget>[
+          Container(color: Colors.red),
+          Container(color: Colors.yellow),
+          Container(color: Colors.green),
+        ],
+      ),
+    );
   }
 }
