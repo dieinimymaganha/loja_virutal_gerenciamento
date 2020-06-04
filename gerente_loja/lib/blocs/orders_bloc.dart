ebce2447e3140ffc2e76b0c0247d8bc3ff4b6335
Dieinimy <dieinimy@gmail.com>
2020-06-04 18:21:53 -0300

    Finalizado a tela de pedidos

--- gerente_loja/lib/blocs/orders_bloc.dart
+++ gerente_loja/lib/blocs/orders_bloc.dart
@@ -2,7 +2,10 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum SortCriteria {READY_FIRST, READY_LAST}
+
 class OrdersBloc extends BlocBase {
+
   final _ordersController = BehaviorSubject<List>();
 
   Stream<List> get outOrders => _ordersController.stream;
@@ -11,20 +14,17 @@ class OrdersBloc extends BlocBase {
 
   List<DocumentSnapshot> _orders = [];
 
+  SortCriteria _criteria;
+
   OrdersBloc(){
     _addOrdersListener();
   }
 
-
-  @override
-  void dispose() {
-    _ordersController.close();
-  }
-
-  void _addOrdersListener() {
-    _firestore.collection('orders').snapshots().listen((snapshot) { 
-      snapshot.documentChanges.forEach((change) {
+  void _addOrdersListener(){
+    _firestore.collection("orders").snapshots().listen((snapshot){
+      snapshot.documentChanges.forEach((change){
         String oid = change.document.documentID;
+
         switch(change.type){
           case DocumentChangeType.added:
             _orders.add(change.document);
@@ -36,11 +36,49 @@ class OrdersBloc extends BlocBase {
           case DocumentChangeType.removed:
             _orders.removeWhere((order) => order.documentID == oid);
             break;
-
         }
       });
 
-      _ordersController.add(_orders);
+      _sort();
     });
   }
+
+  void setOrderCriteria(SortCriteria criteria){
+    _criteria = criteria;
+
+    _sort();
+  }
+
+  void _sort(){
+    switch(_criteria){
+      case SortCriteria.READY_FIRST:
+        _orders.sort((a,b){
+          int sa = a.data["status"];
+          int sb = b.data["status"];
+
+          if(sa < sb) return 1;
+          else if(sa > sb) return -1;
+          else return 0;
+        });
+        break;
+      case SortCriteria.READY_LAST:
+        _orders.sort((a,b){
+          int sa = a.data["status"];
+          int sb = b.data["status"];
+
+          if(sa > sb) return 1;
+          else if(sa < sb) return -1;
+          else return 0;
+        });
+        break;
+    }
+
+    _ordersController.add(_orders);
+  }
+
+  @override
+  void dispose() {
+    _ordersController.close();
+  }
+
 }
